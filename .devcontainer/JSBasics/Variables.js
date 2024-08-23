7329f7f6dd0132ee948bedce93552dcaadd5f57a
paro-mita <141272478+paro-mita@users.noreply.github.com>
2024-08-23 13:06:57 +0000

    container config done

---
+++ .devcontainer/JSBasics/Variables.js
@@ -0,0 +1,17 @@
+const accn_ID = 122334
+let accn_Email = "xyz@gmail.com"
+var accnPswd = "xyz@123"
+accn_city = "Jaipur"
+//try to change constant account Id
+//accn_ID = 144334
+console.log(accn_ID);
+accn_Email="abc@gmail.com"
+console.log(accn_Email);
+accnPswd="abc@2024"
+console.log(accnPswd);
+accn_city="Delhi"
+console.log(accn_city);
+console.table([accn_ID, accn_Email, accnPswd, accn_city]);
+
+
+
