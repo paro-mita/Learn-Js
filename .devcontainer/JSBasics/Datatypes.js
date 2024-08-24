6daf140bf7bc58fd242780b2cdfd974c6ae13ebb
paro-mita <141272478+paro-mita@users.noreply.github.com>
2024-08-24 09:09:57 +0000

    Datatypes

--- .devcontainer/JSBasics/Datatypes.js
+++ .devcontainer/JSBasics/Datatypes.js
@@ -9,6 +9,11 @@ console.table([typeof name, typeof num, typeof isLoggedIn, typeof company, typeo
 // different datatypes
 /*
 Number = int, float, double every number type lies in this type
+Bigint= More big value then number
 String = any name or text within "..."
-
+Boolean = true/false
+null =  standalone value
+undefined = when we don't assign any value to any variable and check its type, it will show "undefined"
+symbol = mostly used in react and used for uniqueness
+object
  */ 
