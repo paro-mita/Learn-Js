914250226fa5210b0ca3a2803e7c9464a3d357f5
paro-mita <141272478+paro-mita@users.noreply.github.com>
2024-08-24 11:01:39 +0000

    Basic Operators

---
+++ .devcontainer/JSBasics/Operators.js
@@ -0,0 +1,34 @@
+//Basic Operators for numbers:
+//Addition : +
+//Subtraction : -
+//Multiply : *
+//Division : /
+//Modulo/Remainder : %
+//Power : **
+
+//Operations on Strings
+
+// let str1 = "hello"
+// let str2 = " Moto"
+// console.log(str1 + str2);
+console.log("1"+2);// o/p = 12
+console.log("1"+"2");// o/p = 12
+console.log(1+ +"2");// o/p = 3
+console.log("1"+2+2);// o/p = 122
+console.log(1+2+"1");// o/p = 31
+
+let str ="2"
+console.log(typeof str);// String
+let strr = +"2"
+console.log(typeof strr);// Number
+
+//similarly
+
+let IsBool = true
+console.log(typeof IsBool);
+let IsBool1 = +true
+console.log(typeof IsBool1);
+
+
+
+
