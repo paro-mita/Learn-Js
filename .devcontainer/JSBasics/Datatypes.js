d33124675f981e7b2154c09b1a05cc00e660b1f4
paro-mita <141272478+paro-mita@users.noreply.github.com>
2024-08-24 08:25:49 +0000

    Datatypes

---
+++ .devcontainer/JSBasics/Datatypes.js
@@ -0,0 +1,14 @@
+//Data types in JS
+let name = "Rishu"
+let num =7
+let isLoggedIn = true
+let company;
+let salary = null;
+
+console.table([typeof name, typeof num, typeof isLoggedIn, typeof company, typeof salary])
+// different datatypes
+/*
+Number = int, float, double every number type lies in this type
+String = any name or text within "..."
+
+ */ 
